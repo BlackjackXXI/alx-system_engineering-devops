00fa24760f2dbb755a2541be6b8c79b26dcb3631
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-22 23:28:38 +0000

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,8 @@
-# Using Puppet, install flask from pip3.
+# Using Puppet, install flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
