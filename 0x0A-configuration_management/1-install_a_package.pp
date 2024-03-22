b1d5b57098eb5d1bb3c361dcdc133620033cb619
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-22 23:19:54 +0000

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,19 @@
-# Install flask from pip3
+# 1-install_a_package.pp
 
+# Ensure python3-pip is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'flask-installer':
+# Install Flask using pip3
+exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+  require => Package['python3-pip'],
+}
+
+# Notify success message
+notify { 'Flask installed successfully':
+  subscribe => Exec['install_flask'],
 }
 
