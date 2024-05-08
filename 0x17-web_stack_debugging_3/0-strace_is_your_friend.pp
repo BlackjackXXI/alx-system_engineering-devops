745e752e3b32ce8d6a4961b212dbfdb0781192b5
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-05-08 21:40:50 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find out why Apache is returning a 500 error and fix it
 
-exec { 'fix config typo':
-  path       => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'error_500_fix':
   command    => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path       => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
