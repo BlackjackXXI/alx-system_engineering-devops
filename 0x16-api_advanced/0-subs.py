9d0f9a56825919fcdedd17ba009fe59bd83ad28c
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-05-08 01:28:56 +0000

    task 1 fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-    """
-    subreddit (str): The name of the subreddit.
-    """
+"""
+fetch the subsribers of a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    return number of subscribers
+        sets the subscriber numer
     """
-    url = f"https://www.reddit.com/r/{}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
-    response = requests.get(url, headers=headers).json()
-    subscribers = response.get('data', {}).get('subscribers')
+
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get('data', {}).get('subscribers')
     if not subscribers:
         return 0
-
     return subscribers
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
