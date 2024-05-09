adf1fd4a6597639f5d348b3180c01ae6c49f51b5
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-05-09 22:04:58 +0000

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,13 +3,19 @@
 Query Reddit API for titles of top ten posts of a given subreddit
 """
 import requests
+
+
 def top_ten(subreddit):
     """
-        fetch to 10
-        if invalid return none
+        return top ten titles for a given subreddit
+        return None if invalid subreddit given
     """
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
+
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     r = requests.get(url, headers=headers).json()
     top_ten = r.get('data', {}).get('children', [])
